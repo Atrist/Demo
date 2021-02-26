9d0cbaae11da858ad515be4f6657b71c1f03110d
atrist <atristmu@outlook.com>
2021-02-26 10:08:29 +0800

    feat: reduce 实现 map filter filter

--- JS/reduce.js
+++ JS/reduce.js
@@ -1,3 +1,43 @@
 /**
  *  使用 reduce 方法实现 forEach、map、filter
  */
+
+// reduce  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+
+// forEach
+
+function forEachUseReduce(array, handler) {
+  array.reduce(function (pre, item, index) {
+    handler(item, index);
+  }, 0);
+}
+
+forEachUseReduce([1, 2, 3, 4], (item) => console.log(item));
+
+// map
+
+function mapUseReduce(array, handler) {
+  let result = [];
+
+  array.reduce(function (pre, item, index) {
+    let mapItem = handler(item, index);
+    result.push(mapItem);
+  }, 0);
+  return result;
+}
+
+console.log(mapUseReduce([1, 2, 3, 4], (item) => item));
+
+// filter
+
+function filterUseReduce(array, handler) {
+  let result = [];
+  array.reduce(function (pre, item, index) {
+    if (handler(item, index)) {
+      result.push(item);
+    }
+  }, 0);
+  return result;
+}
+
+console.log(filterUseReduce([1, 2, 3, 4], (item) => item > 5));
